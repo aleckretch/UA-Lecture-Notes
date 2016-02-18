aba0e1dc54fdd6dc38fc7a31e9a78ec63d412553
Alexander Yee <alexanderyee@email.arizona.edu>
2016-02-18 14:11:14 -0700

    implemented course search

---
+++ courseSearch.php
@@ -0,0 +1,19 @@
+<?php
+  require_once './database.php';
+  $searchKey = $_GET['searchKey'];
+  $arrayOfCourses = Database::searchCourses($searchKey);
+  //var_dump($arrayOfCourses);
+  $JSONArray = '['; //begin JSONArray string
+  foreach($arrayOfCourses as $record) {
+    $courseName = $record['name'];
+    $semester = $record['semester'];
+    $instructor = $record['instructor'];
+    $JSONArray = $JSONArray . '{ "courseName": "' . $courseName . ' ",';
+    $JSONArray = $JSONArray . '"semester": "' . $semester . ' ",';
+    $JSONArray = $JSONArray . '"instructor": "' . $instructor . ' "';
+    $JSONArray = $JSONArray . '},';
+  }
+  $JSONArray = rtrim($JSONArray, ","); //remove last comma
+  $JSONArray = $JSONArray . ']'; //end JSONArray string
+  echo $JSONArray; //echo for use in AJAX in index.html
+?>
